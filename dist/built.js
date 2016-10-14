89c08292d9a11041e25384f3268a5fc34788f054
VariableVasasMT <kritivasas.shukla@mindtickle.com>
2016-10-14 12:23:41 +0530

    improved version 2

--- dist/built.js
+++ dist/built.js
@@ -279,7 +279,7 @@ angular.module('de.stekoe.angular.spotlight', [])
         };
     }]);
 
-angular.module('de.devjs.angular.spotlight')
+angular.module('de.stekoe.angular.spotlight')
     .provider("AngularSpotlight", function () {
         var _iconConfig = iconConfig();
         var _detailsTemplateConfig = detailsTemplateConfig();
