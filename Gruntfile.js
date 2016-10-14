ed6e8eead6a1494234f958ad822f447604011099
VariableVasasMT <kritivasas.shukla@mindtickle.com>
2016-10-14 09:14:12 +0530

    formatting fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,21 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
+	  jshint: {
+      		files: ['Gruntfile.js', 'spotlightOverlay.js', 'provider/*.js', 'directive/*.js'],
+      		options: {
+        		globals: {
+          			jQuery: true
+        		}
+      		}
+    	  },
 	  concat: {
 	    options: {
-	      separator: ';',
 	    },
 	    dist: {
 	      src: ['spotlightOverlay.js', 'provider/spotlight.js', 'directive/*.js'],
 	      dest: 'dist/built.js',
 	    },
-	  },
+	  }
 	});
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.registerTask('default', ['concat']);
